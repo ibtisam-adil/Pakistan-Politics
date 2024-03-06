2bacf9e37f2cb7fadff5f79397076d2b3f94b074
ibtisam-adil <ibtisamadil273@gmail.com>
2024-03-06 17:25:24 +0500

    fixed according to reviewer

---
+++ array.js
@@ -0,0 +1,43 @@
+
+
+let rulerInfo = [
+    {
+        img: "./images/img/parwaiz.jpeg",
+        name: "Musharraf",
+        party: "Retired Pakistani Military",
+        partyDesc: "Pervez Musharraf is a former Pakistani army general and politician who served as President of Pakistan from 1999 to 2008. He came to power through a military coup."
+    },
+    {
+        img: "./images/img/nawaz2.jpeg",
+        name: "Nawaz Sharif",
+        party: "Pakistan Muslim League(N)",
+        partyDesc: "Nawaz Sharif is a Pakistani businessman and politician who served as Prime Minister of Pakistan for three non-consecutive terms. He was removed from office in 2017."
+    },
+    {
+        img: "./images/img/fazal2.jpeg",
+        name: "Fazal ur Rehman",
+        party: "Jamiat Ulema-e-Islam",
+        partyDesc: "Molana Fazlur Rehman is a Pakistani politician known for his conservative views and leadership of the Jamiat Ulema-e-Islam (F) party. He has been a vocal opponent of governm."
+    },
+    {
+        img: "./images/img/imran.jpeg",
+        name: "Imran Khan",
+        party: "Pakistan Tehreek Insaf",
+        partyDesc: "Imran Khan is a Pakistani politician, former cricketer, and current Prime Minister of Pakistan. He founded the PTI party, won the 2018 elections."
+    },
+    {
+        img: "./images/img/zardari.jpeg",
+        name: "Asif Ali Zardari",
+        party: "Pakistan Peoples Party",
+        partyDesc: "Asif Ali Zardari is a Pakistani politician and the co-chairperson of the Pakistan Peoples Party. He served as President of Pakistan from 2008 to 2013 and faced corruption."
+    },
+    {
+        img: "./images/img/imran.jpeg",
+        name: "Imran Khan",
+        party: "Pakistan Tehreek Insaf",
+        partyDesc: "Imran Khan is a Pakistani politician, former cricketer, and current Prime Minister of Pakistan. He founded the PTI party, won the 2018 elections."
+    }
+]
+
+export default rulerInfo;
+
